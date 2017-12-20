378e267fe6e7bc6fabdf8f93385627b1e9ff3a53
montrells <montrell.story@gmail.com>
2017-12-20 09:34:50 -0700

    need work

--- app.js
+++ app.js
@@ -1,13 +1,14 @@
 function addProjects() {
-	var projectDiv = document.getElementById("#ourProjects");
+	var projectDiv = document.getElementById("ourProjects"); //the hash
 	for (var i =0; i < 4; i++) {
 		var project = document.createElement("div");
 		var projectLink = document.createElement("a");
-		var projectImage = document.createElement("img");
+		var projectImage = document.createElement("project0.png");
 		projectLink.setAttribute("href", "http://www.zombo.com");
-		projectImage.setAttribute("src", "project"+1+".png");
+		projectImage.setAttribute("src", "project"+i+".png"); //the I
 		projectLink.appendChild(projectImage);
 		project.appendChild(projectLink);
 		projectDiv.appendChild(project);
+
 	}
 }
